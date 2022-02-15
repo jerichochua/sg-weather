764a5ed77041fb50b46a78ae79a7730c81db7b68
Jericho Chua <jerichochua@gmail.com>
2022-02-15 20:24:36 +0800

    Update how timestamp is handled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+
 import { ForecastList } from './components/ForecastList';
 import { ForecastPeriod } from './components/ForecastPeriod';
 import Header from './components/Header';
 import Footer from  './components/Footer';
-import axios from 'axios';
+
 import './App.css';
+
+// Might not be the most elegant but it suffices for now
+const convertTimestamp = timestamp => {
+    let current_date = new Date(timestamp);
+    let day = current_date.getDate();
+    let month = current_date.getMonth() + 1; // Months start from 0
+    let year = current_date.getFullYear();
+
+    // Make hours/minutes/seconds display double digits (eg: 0 -> 00)
+    // Also not elegant but it will do for now
+    let hrs = (current_date.getHours() < 10 ? '0' : '') + current_date.getHours();
+    let mins = (current_date.getMinutes() < 10 ? '0' : '') + current_date.getMinutes();
+    let secs = (current_date.getSeconds() < 10 ? '0' : '') + current_date.getSeconds();
+
+    return day + "/" + month + "/" + year + " " + hrs + ":" + mins + ":" + secs;
+}
 
 export default class App extends Component {
   constructor(props) {
@@ -23,10 +41,10 @@ export default class App extends Component {
     axios.get('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast')
     .then(resp => {
       this.setState({
-        forecasts: resp.data.items[0].forecasts, 
-        validStartPeriod: new Date(resp.data.items[0].valid_period.start),
-        validEndPeriod: new Date(resp.data.items[0].valid_period.end),
-        forecastLastUpdated: new Date(resp.data.items[0].update_timestamp),
+        forecasts: resp.data.items[0].forecasts,
+        validStartPeriod: convertTimestamp(resp.data.items[0].valid_period.start),
+        validEndPeriod: convertTimestamp(resp.data.items[0].valid_period.end),
+        forecastLastUpdated: convertTimestamp(resp.data.items[0].update_timestamp),
         isFetched: true
       });
     })
