4c7ef0c07c0c2d477e727f794e7a80208201cf92
Jericho Chua <jerichochua@gmail.com>
2022-02-20 21:19:52 +0800

    Remove toLocaleString for forecastLastUpdated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = ({ isFetched, forecastLastUpdated }) => {
     return (
         <footer className="footer">
-            <p>Forecast last updated: {isFetched ? forecastLastUpdated.toLocaleString() : "-"}</p>
+            <p>Forecast last updated: {isFetched ? forecastLastUpdated : "-"}</p>
             <p>Source: <a href="https://data.gov.sg/" target="_blank" rel="noopener noreferrer">data.gov.sg</a></p>
         </footer>
     )
